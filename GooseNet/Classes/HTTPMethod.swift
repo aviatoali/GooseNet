c8834da054e419cc5401d72fbc14d432095eab52
aviatoali <aviatoali@gmail.com>
2019-07-12 14:32:09 -0500

    adding HTTPProtocol and HTTPMethod definitions

---
+++ GooseNet/Classes/HTTPMethod.swift
@@ -0,0 +1,13 @@
+import Foundation
+/**
+ Represents the methods for a web service
+ - author: Ali H. Shah
+ - date: 07/10/2019
+ */
+public enum HTTPMethod: String {
+    case GET
+    case PUT
+    case POST
+    case PATCH
+    case DELETE
+}
---
+++ GooseNet/Classes/HTTPProtocol.swift
@@ -0,0 +1,10 @@
+import Foundation
+/**
+ Represents the different protocols for a webservice.
+ - author: Ali H. Shah
+ - date: 07/10/2019
+ */
+public enum HTTPProtocol: String {
+    case http
+    case https
+}
